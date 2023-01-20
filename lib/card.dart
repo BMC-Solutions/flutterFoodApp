c05d191a5cb9c1839e7aade5cfa4506fa22ebc03
Abdul Qadeer <AbdulQadeerpk321@gmail.com>
2023-01-20 12:35:41 +0500

    icon fixed on home screen

--- lib/card.dart
+++ lib/card.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
 class card{
- IconData icon;
  String imageUrl;
  String title;
  String subtitle;
 
- card(this.icon, this.imageUrl, this.title, this.subtitle);
+ card(this.imageUrl, this.title, this.subtitle);
 }
